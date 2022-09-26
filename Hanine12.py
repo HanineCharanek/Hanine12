29f577ea530a6192ac9a59af3f1d541951c2f11a
HanineCharanek <93096154+HanineCharanek@users.noreply.github.com>
2022-09-26 14:09:53 +0300

    Update Hanine12.py

--- Hanine12.py
+++ Hanine12.py
@@ -150,27 +150,6 @@ st.balloons()
 st.progress(10)
  
     
-st.empty()
-placeholder = st.empty()
-
-# Replace the placeholder with some text:
-placeholder.text("Hello")
-
-# Replace the text with a chart:
-placeholder.line_chart({"data": [1, 5, 2, 6]})
-
-# Replace the chart with several elements:
-with placeholder.container():
-    st.write("This is one element")
-    st.write("This is another")
-
-# Clear all those elements:
-placeholder.empty()
-
-col1, col2 = st.columns(2)
-
-
-
 
 df = pd.DataFrame({
     'first column': ["India", "China", "Brazil", "Ethiopia"],
